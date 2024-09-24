60ddf1758a0ef2236677a3f1fc350e0a75fe338c
Lefie <qiwenruan11@gmail.com>
2024-09-24 15:47:23 -0400

    add some pages. design and implement the landing page

---
+++ app/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+
+  const name = "lemon"
+
+  return (
+    <div className="App">
+      <div className='yellow-bar-top'> </div>
+      <div className='yellow-bar-bottom'></div>
+
+      <div className="blog-name">
+        <h1 className='title'>Blog</h1> 
+        <p className='slogan'>Blog about anything</p>
+      </div>
+
+      <div className='btns'>
+        <div className='btn'>Log In</div>
+        <div className='btn'>Sign In</div>
+        <div className='btn'>Read</div>
+      </div>
+
+     
+
+     
+    </div>
+  );
+}
+
+export default App;
